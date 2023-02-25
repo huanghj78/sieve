f257d99753229bd3eace061f8459257d11d09c7d
huanghj78 <8043008+huanghj78@user.noreply.gitee.com>
2023-02-25 22:46:35 +0800

    fix bug

--- user_server/utils.py
+++ user_server/utils.py
@@ -4,4 +4,4 @@ import subprocess
 def exec_bash(cmd):
     p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, encoding='utf-8')
     p.wait()
-    return p.stdout.read().split('\n')[:-2]
+    return p.stdout.read().split('\n')[:-1]
